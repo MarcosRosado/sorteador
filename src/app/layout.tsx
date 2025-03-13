889e1744d5519e3d230a38c3a2f4cae66e10b9b3
Marcos Rosado <marcos@tacticup.com.br>
2025-03-12 22:18:01 -0300

    add Products page with navigation and button components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 import '@/styles/globals.css';
+import Providers from "@components/providers";
+import {NavItem} from "@components/nav-item";
+import {ClipboardCheck, Dice6, Settings} from "lucide-react";
 
 export const metadata = {
   title: 'Next.js App Router + NextAuth + Tailwind CSS',
@@ -14,8 +17,39 @@ export default function RootLayout({children}: { children: React.ReactNode; })
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"/>
     </head>
     <body className="flex min-h-screen w-full flex-col">
-    {children}
+      <Providers>
+        <main className="flex min-h-screen w-full flex-col bg-muted/40 pl-10">
+          <DesktopNav />
+          <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+            <main className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40">
+              {children}
+            </main>
+          </div>
+        </main>
+      </Providers>
     </body>
     </html>
+  );
+}
+
+
+function DesktopNav() {
+  return (
+    <aside className="fixed inset-y-0 left-0 z-10 w-50 flex-col border-r border-gray-200 bg-background shadow-2xl flex">
+      <nav className="flex flex-col items-start gap-4 px-2 py-5">
+        <NavItem href="/" label="Sortear">
+          <Dice6 className="h-5 w-5" />
+        </NavItem>
+
+        <NavItem href="/generate" label="Gerar Cartelas">
+          <ClipboardCheck className="h-5 w-5" />
+        </NavItem>
+      </nav>
+      <nav className="mt-auto flex flex-col items-start gap-4 px-2 py-5">
+        <NavItem href="/settings" label="Configurações">
+          <Settings className="h-5 w-5" />
+        </NavItem>
+      </nav>
+    </aside>
   );
 }
--- src/components/nav-item.tsx
+++ src/components/nav-item.tsx
@@ -1,11 +1,9 @@
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger
-} from '@/components/tooltip';
+"use client";
+
+import {Tooltip, TooltipContent, TooltipTrigger} from '@components/tooltip';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import {usePathname} from 'next/navigation';
 
 export function NavItem({
                           href,
@@ -24,9 +22,10 @@ export function NavItem({
         <Link
           href={href}
           className={clsx(
-            'flex items-center gap-2 rounded-full text-muted-foreground transition-colors hover:text-foreground hover:bg-gray-200 md:h-8 md:w-full',
+            'flex items-center gap-2 text-muted-foreground transition-colors hover-color rounded-sm hover:rounded-sm hover:p-4 md:h-8 md:w-full',
             {
-              'bg-accent text-black': pathname === href
+              'bg-accent text-black p-4 rounded-sm': pathname === href,
+              'hover:text-center': true // Add this line to center text on hover
             }
           )}
         >
